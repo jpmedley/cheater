2a6956b4279e2a18fe50ead916f8e594b31be164
jmedley@google.com <jmedley@google.com>
2016-06-19 18:38:26 +0200

    Add node and gulp.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['generate-service-worker']);
+
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'app';
+
+  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    stripPrefix: rootDir
+  }, callback);
+});
